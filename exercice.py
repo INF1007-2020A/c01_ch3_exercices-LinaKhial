8a17517dfc1177f4babea24f95641d867925492d
LinaKhial <lina.khial@polymtl.ca>
2020-09-14 17:45:49 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -14,7 +14,8 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    degree = math.degrees(angle_rads) # 
+    degree = math.degrees(angle_rads) # change the radian angle to degree first
+    
     return 0.0, 0.0, 0.0
 
 
