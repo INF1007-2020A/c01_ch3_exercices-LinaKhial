20954b0bfac831d76155522eff926e7002510252
LinaKhial <lina.khial@polymtl.ca>
2020-09-14 18:07:36 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -24,7 +24,7 @@ def to_degrees(angle_rads: float) -> tuple:
 def to_celsius(temperature: float) -> float:
     try :
         if temperature < -459.67 :
-            
+            ra
     return 0.0
 
 
