e4740830a99d137221dfe41a6ff9ec51ce1641c1
LinaKhial <lina.khial@polymtl.ca>
2020-09-14 18:00:17 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -22,11 +22,12 @@ def to_degrees(angle_rads: float) -> tuple:
 
 
 def to_celsius(temperature: float) -> float:
-    
+
     return 0.0
 
 
 def to_farenheit(temperature: float) -> float:
+    
     return 0.0
 
 
